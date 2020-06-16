cb209d9e1e70ae5a9e9b6e4159716bb39f67efc7
Hadi Abedi <habedi71@gmail.com>
2020-06-16 18:30:05 +0430

    Updates

---
+++ precache-manifest.ea689515d324f569bec5d1cafcfbf436.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a164b1855b7207ced03292d4671b7a1e",
+    "url": "/react-face-detection/index.html"
+  },
+  {
+    "revision": "2703df77f0354c2def27",
+    "url": "/react-face-detection/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ef7d6e8d8b61737f3ca3",
+    "url": "/react-face-detection/static/css/main.0f565db1.chunk.css"
+  },
+  {
+    "revision": "2703df77f0354c2def27",
+    "url": "/react-face-detection/static/js/2.051cbc1b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-face-detection/static/js/2.051cbc1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef7d6e8d8b61737f3ca3",
+    "url": "/react-face-detection/static/js/main.2ff6eb79.chunk.js"
+  },
+  {
+    "revision": "3280b5a33f54861cf8a2",
+    "url": "/react-face-detection/static/js/runtime-main.afa51dd6.js"
+  }
+]);
